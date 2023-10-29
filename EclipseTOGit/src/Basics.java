71ece59035d406717dd772613ffdb56808178a81
user <user@DESKTOP-OA2QKU2>
2023-10-29 12:15:13 +0530

    patch version1

--- EclipseTOGit/src/Basics.java
+++ EclipseTOGit/src/Basics.java
@@ -3,6 +3,8 @@ public class Basics {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git Intigration");
+		System.out.println("Intigatating eclips to git");
+		System.out.println("git to jenkisns");
 
 	}
 
